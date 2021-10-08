81a56dec3f1601602bed23f599c983141d29f82a
garza.julian281 <garza.julian281@gamil.com>
2021-10-08 16:52:54 -0500

    checking in first exercise

---
+++ js/external.js
@@ -0,0 +1,24 @@
+console.log("Hello from external JavaScript");
+alert("Welcome to my Website!!")
+
+var color = prompt("What is your favorite color?");
+alert(color + " is my favorite color too!")
+
+
+// var cost = 3;
+// var littleMermaid = prompt("How many days do you want to rent Little Mermaid?");
+// var brotherBear = prompt("How many days do you want to rent Big Bear?");
+// var hercules = prompt("How many days do you want to rent Hercules?");
+// var totalDays = parseInt(littleMermaid) + parseInt(brotherBear) + parseInt(hercules);
+// console.log(totalDays);
+//
+// var totalCost = cost * totalDays;
+// alert("Your total cost is: " + totalCost + " Dollars");
+var totalSum
+var facebook = prompt("How much did you get paid from Facebook?");
+var google = prompt("How much did you get paid from Google?");
+var amazon = prompt("How much did you get paid from Amazon?");
+var totalPay = parseInt(facebook) + parseInt(amazon) + parseInt(google);
+console.log(totalPay)
+
+alert("Total payment from Amazon, Facebook, Google" + totalPay);
