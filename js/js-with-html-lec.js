7332c45056fc846bbdf9132b4dd9f86c58001d71
garza.julian281 <garza.julian281@gamil.com>
2021-10-08 11:21:08 -0500

    pushing lec example

---
+++ js/js-with-html-lec.js
@@ -0,0 +1,19 @@
+// "use strict";
+
+var julian = "julian";
+console.log(julian);
+
+console.log("hello");
+
+var returnOfAlert = alert("TGIF");
+console.log("returnOfAlert");
+
+confirm("plans for weekend");
+
+var returnOfConfirm = confirm("plans?");
+console.log("returnOfConform");
+
+var favoriteNuMetalBand = prompt(message: "What's your favorite nu-metal band?");
+console.log("Ha Ha! you like "  + favoriteNuMetalBand + "!" );
+
+
