464a87aa415b47f520560a2a87192c667e28d8aa
garza.julian281 <garza.julian281@gamil.com>
2021-10-15 13:42:08 -0500

    pushing files request by Bianca

---
+++ js/warmup_exercises.js
@@ -0,0 +1,20 @@
+(function () {
+
+
+
+
+function returnSeven() {
+    return 7;
+}
+    consolelog(returnSeven());
+
+    if (false) {
+        console.log("Am I Seen?");
+}
+    var hadBreakfast = Boolean(Math.round(Math.random()));`
+    
+    var hadBreakfastMessage = (hadBreakfast) ? "Thank you breakfast was good." : "No breakfast".;
+console.log(hasBreakfast);
+console.log(hasBreakfastMessage);
+
+})();
