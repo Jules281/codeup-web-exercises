ee071205b34c28edb1be71c9f66c51ef120d4009
garza.julian281 <garza.julian281@gamil.com>
2021-10-22 10:25:49 -0500

    upload

--- js/warmup_exercises.js
+++ js/warmup_exercises.js
@@ -3,18 +3,36 @@
 
 
 
-function returnSeven() {
-    return 7;
-}
-    consolelog(returnSeven());
+// function returnSeven() {
+//     return 7;
+// }
+//     consolelog(returnSeven());
+//
+//     if (false) {
+//         console.log("Am I Seen?");
+// }
+//     var hadBreakfast = Boolean(Math.round(Math.random()));`
+//
+//     var hadBreakfastMessage = (hadBreakfast) ? "Thank you breakfast was good." : "No breakfast".;
+// console.log(hasBreakfast);
+// console.log(hasBreakfastMessage);
 
-    if (false) {
-        console.log("Am I Seen?");
-}
-    var hadBreakfast = Boolean(Math.round(Math.random()));`
-    
-    var hadBreakfastMessage = (hadBreakfast) ? "Thank you breakfast was good." : "No breakfast".;
-console.log(hasBreakfast);
-console.log(hasBreakfastMessage);
 
+    // Write a function named 'moveToEnd' that takes in an array, and returns
+    // the array with the original first index moved to the last index of the array.
+
+        Example:
+    moveToEnd([1, 2, 3, 4]) 		  // returns [2, 3, 4, 1];
+    moveToEnd(['roll', 'rock', 'and']  // returns ['rock', 'and', 'roll'];
+
+
+function moveToEnd(arr[1,2,3,4]) {
+        var removedElement = arr.shift();
+        arr.push(removedElement)
+        return arr
+
+}
+console.log(moveToEnd(arr[1,2,3,4]))
+    console.log(moveToEnd())
 })();
+
